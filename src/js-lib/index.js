be7a5c7480ca08f4e4b339c78c8fdba744ecad35
Jeremy Chone <jeremy.chone@gmail.com>
2017-05-22 18:04:44 -0700

    . fix home card layout

--- src/js-lib/index.js
+++ src/js-lib/index.js
@@ -1,7 +1,9 @@
 var Handlebars = require("handlebars/runtime")["default"];
 var mvdom = require("mvdom");
+var gtx = require("gtx");
 
 if (window){
 	window.Handlebars = Handlebars;
 	window.mvdom = mvdom;
+	window.Gtx = gtx;
 }
