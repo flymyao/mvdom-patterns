2f8d74ad9845057844593b8e4a6601b6f7b366cc
Jeremy Chone <jeremy.chone@gmail.com>
2017-07-10 12:45:08 -0700

    . scaffold for the validators module

---
+++ src/js-app/validator.js
@@ -0,0 +1,31 @@
+// IMPORTANT: Just the scaffolding for now. Not Implemented. 
+
+var _validators = {};
+
+module.exports = new Validator();
+
+
+
+function Validator(){
+
+}
+
+// --------- Public Methods --------- //
+Validator.prototype.validate = function(validationError){
+
+};
+
+Validator.prototype.add = function(name, ruleOptions){
+
+};
+
+Validator.prototype.catch = function(validationError){
+
+};
+// --------- /Public Methods --------- //
+
+
+
+// --------- Private Methods --------- //
+
+// --------- /Private Methods --------- //
